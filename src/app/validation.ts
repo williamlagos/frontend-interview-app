a93016e6b4a5c30de4789cc9bd2413c4985de647
William Oliveira de Lagos <william.lagos@icloud.com>
2023-08-25 17:14:12 +0200

    feat: added separate validation module to enable the use with other types of steps and validations

---
+++ src/app/validation.ts
@@ -0,0 +1,32 @@
+const validateEmail = (email: string) => (
+  !email.toLowerCase().match(
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  ) ? 'The field was filled with an invalid e-mail' : ''
+)
+
+const validateName = (name: string) => name.length < 3 ? 'The name was filled with too few characters' : ''
+
+const validateAge = (age: number) => age < 18 ? 'The age must be 18 or older' : ''
+
+export const validateValues = (inputValues: Record<string, any>, required: boolean): string => {
+  let value = ''
+  const values = Object.values(inputValues)
+  if (required && values.length === 0) {
+    value = 'The field(s) is(are) empty'
+  } else {
+    values.forEach(({ type: inputType, value: inputValue }) => {
+      switch (inputType) {
+        case 'text':
+          value = validateName(inputValue)
+          break
+        case 'email':
+          value = validateEmail(inputValue)
+          break
+        case 'number':
+          value = validateAge(inputValue)
+          break
+      }
+    })
+  }
+  return value
+}
--- src/components/InputStep/InputStep.tsx
+++ src/components/InputStep/InputStep.tsx
@@ -1,18 +1,11 @@
 import React, { useState } from 'react'
+import { validateValues } from '../../app/validation'
 
 interface InputStepProps {
   cb: (field: string, value: Record<string, any>) => void
   entries: Record<string, any>
   required: boolean
   slug: string
-}
-
-const validateEmail = (email: string) => {
-  return String(email)
-    .toLowerCase()
-    .match(
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    )
 }
 
 const InputStep: React.FC<InputStepProps> = (props) => {
@@ -41,35 +34,13 @@ const InputStep: React.FC<InputStepProps> = (props) => {
       </div>
       <div style={{ color: "red" }}>{validationMsg}</div>
       <button onClick={() => {
-        let validation = ''
-        if (props.required && Object.values(inputValues).length === 0) {
-          validation = 'The field(s) is(are) empty'
+        const validation = validateValues(inputValues, props.required)
+        if (validation.length === 0) {
+          props.cb(props.slug, inputValues)
+          setValidationMsg('')
+          setInputValues({}) 
         } else {
-          Object.values(inputValues).forEach(({ type: inputType, value: inputValue }) => {
-            switch (inputType) {
-              case 'text':
-                if (isNaN(inputValue) && String(inputValue).length < 3) {
-                  validation = 'The name was filled with too few characters'
-                }
-                break
-              case 'email':
-                if (isNaN(inputValue) && !validateEmail(String(inputValue))) {
-                  validation = 'The field was filled with an invalid e-mail'
-                }
-                break
-              case 'number':
-                if (!isNaN(inputValue) && inputValue < 18) {
-                  validation = 'The age must be 18 or older'
-                }
-            }
-          })
-          if (validation.length === 0) {
-            props.cb(props.slug, inputValues)
-            setValidationMsg('')
-            setInputValues({}) 
-          } else {
-            setValidationMsg(validation)
-          }
+          setValidationMsg(validation)
         }
       }}>Next</button>
     </>
