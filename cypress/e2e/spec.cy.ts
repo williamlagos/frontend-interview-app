83772cef4bb2b70f0436a02cf1b6048c995d255a
William Oliveira de Lagos <william.lagos@icloud.com>
2023-08-28 11:57:46 +0200

    feat: added developer insurance flow e2e success path test and inserted identifiers structure

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,21 +1,51 @@
-describe('tests designer insurance flow', () => {
+const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:3000'
+
+const identifiers = {
+  designerStart: '[data-cy="designer-start"]',
+  developerStart: '[data-cy="developer-start"]',
+  firstNameInput: '[data-cy="fname-input"]',
+  lastNameInput: '[data-cy="lname-input"]',
+  emailInput: '[data-cy="email-input"]',
+  ageInput: '[data-cy="age-input"]',
+  nextStep: '[data-cy="next-step"]',
+  purchaseEnd: '[data-cy="purchase-end"]'
+}
+
+describe('tests developer insurance flow', () => {
   it('passes', () => {
-    cy.visit('http://localhost:3000')
-    cy.get('[data-cy="designer-start"]').click()
+    cy.visit(FRONTEND_URL)
+    cy.get(identifiers.developerStart).click()
 
     // E-mail success path step
-    cy.get('[data-cy="email-input"]').type('william.lagos@icloud.com')
-    cy.get('[data-cy="next-step"]').click()
+    cy.get(identifiers.emailInput).type('william.lagos@icloud.com')
+    cy.get(identifiers.nextStep).click()
 
     // Age success path step
-    cy.get('[data-cy="age-input"]').type(25)
-    cy.get('[data-cy="next-step"]').click()
+    cy.get(identifiers.ageInput).type(25)
+    cy.get(identifiers.nextStep).click()
+
+    cy.get(identifiers.purchaseEnd).click()
+  })
+})
+
+describe('tests designer insurance flow', () => {
+  it('passes', () => {
+    cy.visit(FRONTEND_URL)
+    cy.get(identifiers.designerStart).click()
+
+    // E-mail success path step
+    cy.get(identifiers.emailInput).type('william.lagos@icloud.com')
+    cy.get(identifiers.nextStep).click()
 
     // Age success path step
-    cy.get('[data-cy="fname-input"]').type('William')
-    cy.get('[data-cy="lname-input"]').type('Oliveira de Lagos')
-    cy.get('[data-cy="next-step"]').click()
+    cy.get(identifiers.ageInput).type(25)
+    cy.get(identifiers.nextStep).click()
 
-    cy.get('[data-cy="purchase-end"]').click()
+    // Age success path step
+    cy.get(identifiers.firstNameInput).type('William')
+    cy.get(identifiers.lastNameInput).type('Oliveira de Lagos')
+    cy.get(identifiers.nextStep).click()
+
+    cy.get(identifiers.purchaseEnd).click()
   })
 })
